f2e160647cc6d23889326221ceca6cb337dcdfd2
KumarVikky <vikky.kumar270@gmail.com>
2024-04-16 11:23:08 +0530

    Manual dom implemented

--- force-app/main/default/lwc/cv_MyResumeSummary/cv_MyResumeSummary.js
+++ force-app/main/default/lwc/cv_MyResumeSummary/cv_MyResumeSummary.js
@@ -2,4 +2,8 @@ import { LightningElement, api } from 'lwc';
 
 export default class Cv_MyResumeSummary extends LightningElement {
     @api details
+
+    renderedCallback(){
+        this.template.querySelector('.manualContent').innerHTML = this.details.DESCRIPTION;
+    }
 }
